66f9b670408a3238ff0ab3f9ca21c52ed25553be
MoYuanJun <moyuanjun925@gmail.coml>
2023-10-03 12:19:52 +0800

    init

---
+++ .stylelintrc.js
@@ -0,0 +1,5 @@
+const path = require("path");
+
+module.exports = {
+  extends: [path.resolve(__dirname, './client/node_modules/@kunlunxu/create-react-app/.stylelintrc.js')],
+};
---
+++ client/.projectrc.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import Home from './pages/Home'
+
+
+export default {
+  logo: {             
+    img: void 0,      
+    title: 'Chrome117',
+  },
+  footer: () => ('footer'),
+  header: () => ('header'),
+  useHashRouter: true,
+  menu: [ 
+    {
+      key: 'home',
+      title: '首页',
+      icon: 'AndroidOutlined',
+      url: '/',
+      routes: [
+        {
+          path: '/',
+          element: <Home />,
+        },
+      ],
+    },
+  ],
+};
